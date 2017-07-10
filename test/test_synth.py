f15d5ab665dce8503a20a1634dd27b88b4792a61
mcsnowhammer <mattias.hammar@gmail.com>
2017-07-10 21:04:37 +0200

    Added more tests

--- test/test_synth.py
+++ test/test_synth.py
@@ -3,12 +3,12 @@ import unittest
 import config
 import audio
 import synth
-
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 class TestSynth(unittest.TestCase):
     def setUp(self):
+        super(TestSynth, self).setUp()
         self._synth = synth.Synth()
 
     def test_pwm_wave(self):
@@ -24,6 +24,16 @@ class TestSynth(unittest.TestCase):
     def test_saw_wave(self):
         sound = self._synth.get_saw_wave(80, 1.5)
         self.assertEqual(sound.size, 1.5 * config.SAMPLE_RATE)
+        self._playSound(sound)
+
+    def test_saw_pwm_wave(self):
+        sound = self._synth.get_saw_pwm_wave(100, 1.5, 2.0, 1.0)
+        self.assertEqual(sound.size, 1.5 * config.SAMPLE_RATE)
+        self._playSound(sound)
+
+    def test_square_wave(self):
+        sound = self._synth.get_square_wave(50, 1.6, 0.5)
+        self.assertEqual(sound.size, 1.6 * config.SAMPLE_RATE)
         self._playSound(sound)
 
     def _playSound(self, sound):
